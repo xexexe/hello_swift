7ff10c523ebe48a29093917a17b6bffee460fca5
Mark Ran <ranjun@wondershare.com>
2017-05-10 11:41:16 +0800

    add uitesting

--- Demo/BeginProgect/BeginProgectUITests/BeginProgectUITests.swift
+++ Demo/BeginProgect/BeginProgectUITests/BeginProgectUITests.swift
@@ -31,6 +31,8 @@ class BeginProgectUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCUIApplication().buttons["Button"].tap()
+        
     }
     
 }
