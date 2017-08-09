bc6073a8a372207e9e957715305e6c56e91048f5
ranjun <ranjun@wondershare.cn>
2017-08-09 11:03:51 +0800

    create a demo for WKWebView

---
+++ Demo/WKWebView/WKWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKWebView
+//
+//  Created by Mark Ran on 2017/8/9.
+//  Copyright © 2017年 hehe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
