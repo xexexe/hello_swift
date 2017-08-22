13b5353e72a1ba1c88ba2dbc1d60f6e415fb35a6
ranjun <ranjun@wondershare.cn>
2017-08-22 17:18:33 +0800

    create a demo for difference game

---
+++ Demo/BasicMacDemo/BasicMacDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BasicMacDemo
+//
+//  Created by Mark Ran on 2017/8/18.
+//  Copyright © 2017年 hehe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Demo/DiffGame/DiffGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiffGame
+//
+//  Created by Mark Ran on 2017/8/22.
+//  Copyright © 2017年 markran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
