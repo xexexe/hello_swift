192f54befef700680d16c2e91bafae42f419ce59
ranjun <ranjun@wondershare.cn>
2017-09-19 13:44:29 +0800

    add change

--- Demo/RegularExpression/RegularExpression/ViewController.swift
+++ Demo/RegularExpression/RegularExpression/ViewController.swift
@@ -12,33 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-//        let pattern = "^13$"
-//        let str = "lsdjlfafjllf1357892131231546143698wdaf"
-//        check(pattern: pattern, str: str)
-//        let mailPattern = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$"
-//        let mailStr = "lsls@oodd.com"
-//        check(pattern: mailPattern, str: mailStr)
-        let hehe = URLRegex(text: "sjdflasjdflweojfoemflfsajhttp://2233de.jpg")
-        print(hehe)
-        
-//        if let path = Bundle.main.path(forResource: "hehe", ofType: "html") {
-////            let pattern = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$"
-//////            let pattern = "^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$"
-//////            let pattern = "^1[0-9]{10}$"
-//////            let pattern = "^[a-z0-9_-]{3,16}$"
-//            let input = try?String.init(contentsOfFile: path, encoding: .utf8)
-////            let input = "admin@hangge.com"
-////            
-////            let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)
-////            
-////            let matches = regex?.matches(in: input,
-////                                                 options: [],
-////                                                 range: NSMakeRange(0, input.characters.count))
-//
-//            check(str: input!)
-//        }
+        test1()
+        test2()
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,6 +32,91 @@ class ViewController: UIViewController {
         } catch {
             print(error)
         }
+    }
+    
+    private func old() {
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //        let pattern = "^13$"
+        //        let str = "lsdjlfafjllf1357892131231546143698wdaf"
+        //        check(pattern: pattern, str: str)
+        //        let mailPattern = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$"
+        //        let mailStr = "lsls@oodd.com"
+        //        check(pattern: mailPattern, str: mailStr)
+        
+        
+        
+        //        if let path = Bundle.main.path(forResource: "hehe", ofType: "html") {
+        ////            let pattern = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$"
+        //////            let pattern = "^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$"
+        //////            let pattern = "^1[0-9]{10}$"
+        //////            let pattern = "^[a-z0-9_-]{3,16}$"
+        //            let input = try?String.init(contentsOfFile: path, encoding: .utf8)
+        ////            let input = "admin@hangge.com"
+        ////
+        ////            let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+        ////
+        ////            let matches = regex?.matches(in: input,
+        ////                                                 options: [],
+        ////                                                 range: NSMakeRange(0, input.characters.count))
+        //
+        //            check(str: input!)
+        //        }
+    }
+    
+    private func test1() {
+        
+        //let hehe = URLRegex(text: "sjdflasjdflweojfoemflfsajhttp://2233de.jpg")
+        //print(hehe)
+        
+        /*
+         NSString *phoneNo = @"13143503442";
+         NSRange range = [phoneNo rangeOfString:@"^1[3]\\d{9}$" options:NSRegularExpressionSearch];
+         if (range.location != NSNotFound) {
+         NSLog(@"%@", [phoneNo substringWithRange:range]);
+         }
+         */
+        let phoneNo: NSString = "13143503442"
+        let range = phoneNo.range(of: "^1[3]\\d{9}$", options: NSString.CompareOptions.regularExpression)
+        print(range.location)
+    }
+    
+    private func test2() {
+        /*
+        NSString *url = @"1229436624@qq.com";
+        NSError *error;
+        // 创建NSRegularExpression对象并指定正则表达式
+        NSRegularExpression *regex = [NSRegularExpression
+            regularExpressionWithPattern:@"[^@]*\\."
+            options:0
+            error:&error];
+        if (!error) { // 如果没有错误
+            // 获取特特定字符串的范围
+            NSTextCheckingResult *match = [regex firstMatchInString:url
+                options:0
+                range:NSMakeRange(0, [url length])];
+            if (match) {
+                // 截获特定的字符串
+                NSString *result = [url substringWithRange:match.range];
+                NSLog(@"%@",result);
+            }
+        } else { // 如果有错误，则把错误打印出来
+            NSLog(@"error - %@", error);
+        } 
+         */
+        
+        let url: NSString = "122943@sss.6624@qq.com"
+        do {
+            let regex = try NSRegularExpression(pattern: "[^@]*\\.", options: NSRegularExpression.Options(rawValue: 0))
+            let match = regex.matches(in: url as String, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, url.length))
+            print(match)
+            for m in match {
+                print(url.substring(with: m.range))
+            }
+        } catch  {
+            print(error)
+        }
+        
     }
 
     private func check(str: String) {
